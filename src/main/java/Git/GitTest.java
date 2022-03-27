80ba097a62fedc11601834bdf67405380cb58e8c
WEI <2225825488@qq.com>
2022-03-27 17:09:38 +0800

    first commit

---
+++ src/main/java/Git/GitTest.java
@@ -0,0 +1,11 @@
+package Git;
+
+/**
+ * @author Wei
+ * @create 2022/03/27  17:06
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
